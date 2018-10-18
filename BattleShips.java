629350a72ac94a3fff8440ed77d14188fff410dc
StealtyPanda <42720778+StealthyPanda@users.noreply.github.com>
2018-10-18 17:47:41 +0400

    hmm

--- BattleShips.java
+++ BattleShips.java
@@ -3,7 +3,9 @@
 class BattleShips{
 	public static void main(String[] args) {
 
+		Board b = new Board(2, 3);
 
+		
 
 
 		
--- Board.java
+++ Board.java
@@ -2,12 +2,41 @@
 
 class Board{
 
-	int rows = 0;
-	int columns = 0;
+	int r = 0;
+	int c = 0;
+	String[][] rows;
+	String[] columns;
 
 	public Board(int x, int y){
-		rows = x;
-		columns = y;
+		r = x;
+		c = y;
+
+		rows = new String[r][];
+		columns = new String[c];
+
+		for (int i = 0; i < r; i++) {
+
+			rows[i] = columns;
+			
+		}
 	}
+
+	void display(){
+
+		for (int i = 0; i < r; i++){
+
+			for (int x = 0; x < c; x++) {
+
+				String[][] cr = rows[i];
+				System.out.println(cr[x]);
+				
+			}
+		}
+
+	}
+
+
+
+
 
 }
