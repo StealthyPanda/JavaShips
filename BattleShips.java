1f80974c5e2bf7ab7165cfa4c845f4002054e24e
StealtyPanda <42720778+StealthyPanda@users.noreply.github.com>
2018-10-18 17:26:28 +0400

    First additon just setting files up

---
+++ BattleShips.java
@@ -0,0 +1,13 @@
+//package BattleShips;
+
+class BattleShips{
+	public static void main(String[] args) {
+
+
+
+
+		
+	}
+
+
+}
---
+++ Board.java
@@ -0,0 +1,13 @@
+//package BattleShips;
+
+class Board{
+
+	int rows = 0;
+	int columns = 0;
+
+	public Board(int x, int y){
+		rows = x;
+		columns = y;
+	}
+
+}
