31468bd8e603c890ab088f088133fc2d3f09fafe
StealtyPanda <42720778+StealthyPanda@users.noreply.github.com>
2018-10-21 17:57:16 +0400

    Update BattleShips.java

--- BattleShips.java
+++ BattleShips.java
@@ -4,15 +4,29 @@ class BattleShips{
 	public static void main(String[] args) {
 
 
-		Board b = new Board(3, 4);
-		b.setchar('X');
-		b.display();
-		System.out.println();
-		b.setchar('O', 1, 2);
-		b.display();
-		System.out.println(b.getchar(2, 2));
+		
 
 		
+	}
+
+
+}
+
+
+class BattleBoard{
+
+	private Board b_board;
+
+	public BattleBoard(int x, int y){
+
+		b_board = new Board(x, y);
+
+	}
+
+	public BattleBoard(){
+
+		b_board = new Board(5, 5);
+
 	}
 
 
